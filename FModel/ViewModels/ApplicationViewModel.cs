685ea4110f8b8e2ccc9b3a56cf7fb7614b55d93a
simon <107362910+simonhxd@users.noreply.github.com>
2026-08-09 01:06:02 -0700

    Blevin Model

--- FModel/ViewModels/ApplicationViewModel.cs
+++ FModel/ViewModels/ApplicationViewModel.cs
@@ -79,7 +79,7 @@ public class ApplicationViewModel : ViewModel
     public CopyCommand CopyCommand => _copyCommand ??= new CopyCommand(this);
     private CopyCommand _copyCommand;
 
-    public string InitialWindowTitle => $"FModel";//({Constants.APP_SHORT_COMMIT_ID} - {Constants.APP_BUILD_DATE:MMM d, yyyy})";
+    public string InitialWindowTitle => $"BlevModel";//({Constants.APP_SHORT_COMMIT_ID} - {Constants.APP_BUILD_DATE:MMM d, yyyy})";
     public string GameDisplayName => CUE4Parse.Provider.GameDisplayName ?? "Unknown";
     public string TitleExtra => $"{UserSettings.Default.CurrentDir} ({UserSettings.Default.CurrentDir.UeVersion}){(Build != EBuildKind.Release ? $" ({Build})" : "")}";
 
